a22413b657adc8ef0471acc5abd4f73ebd3985d5
Byeong-gil Jun <junbg@hanyang.ac.kr>
2021-12-29 18:14:17 +0900

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,12 +4,11 @@
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        if (n  % 2 == 1) {
+    public static int nextNumber(int n ) {
+        if (n  % 2 == 1)
             return 3 * n + 1;
-        } else {
+        else
             return n / 2;
-        }
     }
 
     public static void main(String[] args) {
