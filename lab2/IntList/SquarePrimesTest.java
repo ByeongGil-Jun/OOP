359198931c4fa13599ddedf27ee69c2dfa3cedd0
Byeong-gil Jun <junbg@hanyang.ac.kr>
2022-01-12 17:03:56 +0900

    lab4 complete

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,20 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesinclude2() {
+        IntList lst = IntList.of(2, 4, 7, 8, 53, 73, 88);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 4 -> 49 -> 8 -> 2809 -> 5329 -> 88", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesFalse() {
+        IntList lst = IntList.of(4, 4, 16, 8, 6, 12, 24);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 4 -> 16 -> 8 -> 6 -> 12 -> 24", lst.toString());
+        assertFalse(changed);
+    }
 }
