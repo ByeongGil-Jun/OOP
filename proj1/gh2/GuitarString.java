29f8f791a2b3c66b757fbb5f89f3e9b13c751c7f
Byeong-gil Jun <junbg@hanyang.ac.kr>
2022-02-04 22:54:19 +0900

    complete guitar, now create MaxArrayDeque

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,7 +1,9 @@
 package gh2;
 
 // TODO: uncomment the following import once you're ready to start this portion
+import deque.ArrayDeque;
 import deque.Deque;
+import deque.LinkedListDeque;
 // TODO: maybe more imports
 
 //Note: This file will not compile until you complete the Deque implementations
@@ -22,6 +24,11 @@ public class GuitarString {
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your should initially fill your buffer array with zeros.
+        int capacity = (int) Math.round(SR  / frequency);
+        buffer = new ArrayDeque<Double> ();
+        for(int i = 0; i < capacity; i++) {
+            buffer.addLast((double) 0);
+        }
     }
 
 
@@ -35,6 +42,14 @@ public class GuitarString {
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
         //       Math.random() - 0.5 to generate new random numbers for each array index.
+        int capacity = buffer.size();
+        for(int i = 0; i < capacity; i++) {
+            buffer.removeLast();
+        }
+        for(int i = 0; i < capacity; i++) {
+            double r = Math.random() - 0.5;
+            buffer.addLast(r);
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
@@ -44,12 +59,15 @@ public class GuitarString {
         // TODO: Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       **Do not call StdAudio.play().**
+        double t = buffer.removeFirst();
+        t = (t + sample()) / 2;
+        buffer.addLast(t);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
         // TODO: Return the correct thing.
-        return 0;
+        return buffer.get(1);
     }
 }
     // TODO: Remove all comments that say TODO when you're done.
