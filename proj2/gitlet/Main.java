84bc926a73969f55367a9fa3f2e1b05e20d00013
Byeong-gil Jun <junbg@hanyang.ac.kr>
2022-02-15 15:38:27 +0900

    Proj2 start!

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,7 +1,7 @@
 package gitlet;
 
 /** Driver class for Gitlet, a subset of the Git version-control system.
- *  @author TODO
+ *  @author Gily
  */
 public class Main {
 
@@ -10,6 +10,10 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO: what if args is empty?
+        if (args.length == 0) {
+            System.out.println("Please enter a command");
+            System.exit(0);
+        }
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
